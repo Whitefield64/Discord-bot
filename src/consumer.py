0d86e425bfc92491f17d32cd4727f02e84ff7bd8
GregorioMonari <102193750+GregorioMonari@users.noreply.github.com>
2023-04-04 16:34:23 +0200

    environment-variables-configuration

--- src/consumer.py
+++ src/consumer.py
@@ -3,14 +3,59 @@ import discord
 from sepy.SAPObject import *
 from sepy.SEPA import SEPA
 import json
+import os
 
-mySAP = open("./Resources/jsap_test.jsap", 'r')
+first_results=1
+print("#####################")
+print("DISCORD CONSUMER v0.1")
+print("#####################")
+
+#-----MAIN---------------------------------------------------------------
+mySAP = open("Resources/jsap_test.jsap", 'r')
 _JSAP = json.load(mySAP)
-
+print("####################################################################")
+print("- Jsap loaded, overriding configuration")
+# OVERRIDE VARIABLES
+try:
+    _JSAP["host"]=os.environ['HOST_NAME']
+    print("- Env variable 'HOST_NAME' set with value: "+str(_JSAP["host"]))
+except:
+    print("- Env variable 'HOST_NAME' not set, using default: "+str(_JSAP["host"]))
+finally:
+    pass
+try:
+    _JSAP["sparql11protocol"]["port"]=os.environ['HTTP_PORT']
+    print("- Env variable 'HTTP_PORT' set with value: "+str(_JSAP["sparql11protocol"]["port"]))
+except:
+    print("- Env variable 'HTTP_PORT' not set, using default: "+str(_JSAP["sparql11protocol"]["port"]))
+finally:
+    pass
+try:
+    _JSAP["sparql11seprotocol"]["availableProtocols"]["ws"]["port"]=os.environ['WS_PORT']
+    print("- Env variable 'WS_PORT' set with value: "+str(_JSAP["sparql11seprotocol"]["availableProtocols"]["ws"]["port"]))
+except:
+    print("- Env variable 'WS_PORT' not set, using default: "+str(_JSAP["sparql11seprotocol"]["availableProtocols"]["ws"]["port"]))
+finally:
+    pass
+try:
+    _JSAP['extended']['discordConfig']['CHANNEL_ID']=os.environ['CHANNEL_ID']
+    print("- Env variable 'CHANNEL_ID' set with value: "+str(_JSAP['extended']['discordConfig']['CHANNEL_ID']))
+except:
+    print("- Env variable 'CHANNEL_ID' not set, using default: "+str(_JSAP['extended']['discordConfig']['CHANNEL_ID']))
+finally:
+    pass
+try:
+    _JSAP['extended']['discordConfig']['TOKEN']=os.environ['TOKEN']
+    print("- Env variable 'TOKEN' set with value: "+str(_JSAP['extended']['discordConfig']['TOKEN'] ))
+except:
+    print("- Env variable 'TOKEN' not set, using default: "+str(_JSAP['extended']['discordConfig']['TOKEN'] ))
+finally:
+    pass
+print("####################################################################")
 
 
 #CONFIGURAZIONE DISCORD
-CHANNEL_ID = _JSAP['extended']['discordConfig']['CHANNEL_ID']    
+CHANNEL_ID = int(_JSAP['extended']['discordConfig']['CHANNEL_ID'])    
 TOKEN = _JSAP['extended']['discordConfig']['TOKEN'] 
 intents = discord.Intents.default()
 intents.members = True
@@ -45,13 +90,17 @@ Content:
 """
 #------------------------------------------------------------------------------------
 
-first_results=1
+#first_results=1
 def on_notification(a,r):
+    global first_results
     if first_results == 1:
         print("Ignored first results")
         first_results=0
+        
+        #print(first_results)
     else:
         print(discord_message_data(a))
+        print(CHANNEL_ID)
         canale = bot.get_channel(CHANNEL_ID)
         bot.loop.create_task(canale.send(discord_message_data(a)))
 
